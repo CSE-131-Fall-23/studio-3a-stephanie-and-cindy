3b247b4e77d6672be5c9d8749ded112aa6c11092
TheStormfly <TheStormfly@localhost>
2023-09-20 15:52:22 -0500

    I made an array

--- src/studio3/Seive.java
+++ src/studio3/Seive.java
@@ -7,10 +7,16 @@ public class Seive {
 		Scanner in = new Scanner(System.in);
 		System.out.println("Enter n.");
 		int n = in.nextInt();
-		int[] seive = new int[n+1];
+		boolean[] seive = new boolean[n+1];
 		{
 			for (int i=0; i < n+2 ; i++) 
 			{
-				System.out.print(seive[i]);
+				seive[i] = (i % 2 > 0) || i==2;
+				seive[i] = (i % 3 > 0) || i==3;
+				if(seive[i]= true);
+				{
+					System.out.println(i);
+				}
 			}
-		}}}
+		}
+	}}}
